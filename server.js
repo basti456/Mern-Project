c428a0aca4c81daf87677daf72fa9b420163ce3f
basti456 <ekagraagrawal49@gmail.com>
2023-03-13 15:25:51 +0530

    json files added

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-const http = require("http");
+const express = require("express");
+const {users}=require("./data/users.json")
+const app = express();
+
+app.use(express.json());
+
 const port = 8081;
 
-http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h2>Hey Server started and work here</h2>");
-    res.end();
-}).listen(port, () => {
-    console.log(`Server started at port ${port}`);
+/**
+ * Route:"/users"
+ * Method:GET
+ * Desc:Get all the users
+ * Access:public
+ * params:None
+ */
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users
+    });
 });
+app.get("/", (req, res) => {
+    res.status(200).json({
+        "message":"Server is started"
+    });
+});
+app.listen(port, () => {
+    console.log(`Server started at the port ${port}`);
+})
