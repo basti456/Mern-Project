4bfec82e2a227d6125836a30cee3ef4e3cf394ac
basti456 <ekagraagrawal49@gmail.com>
2023-03-18 17:31:33 +0530

    New commit

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const { books } = require("../data/books.json");
 
+const { users } = require("../data/users.json");
+
 /**
  * Route:"/books"
  * Method:GET
@@ -27,6 +29,8 @@ router.get("/", (req, res) => {
  * Access:public
  * params:id
  */
+
+
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const book = books.find((each) => each.id === id);
@@ -35,13 +39,51 @@ router.get("/:id", (req, res) => {
       success: false,
       data: "Book Doesn't exists",
     });
-  } else {
-    return res.status(200).json({
-      success: true,
-      message: "Book Found",
-      data: book,
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Book Found",
+    data: book,
+  });
+});
+
+
+/**
+ * Route:"/books/issued"
+ * Method:GET
+ * Desc:Get a issued books
+ * Access:public
+ * params:none
+ */
+
+router.get("/issued/by-user", (req, res) => {
+  const userWithIssuedBook = users.filter((each) => {
+    if (each.issuedBook) {
+      return each;
+    }
+  });
+  const issuedBooks = [];
+  userWithIssuedBook.forEach((each) => {
+    const id = each.issuedBook;
+    const book = books.find((eachBook) => eachBook.id === id);
+
+    book.issuedBy = each.name;
+    book.issuedDate = each.issuedDate;
+    book.returnDate = each.returnDate;
+    issuedBooks.push(book);
+  });
+  if (issuedBooks.length === 0) {
+    return res.status(404).json({
+      success: false,
+      message: "No issue books found",
     });
   }
+  return res.status(200).json({
+    success: true,
+    message: "Issued books fetched successfully",
+    data:issuedBooks
+  });
 });
+
 
 module.exports = router;
